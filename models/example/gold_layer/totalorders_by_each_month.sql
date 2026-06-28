1f845f044e033137f1786cfcd8bd1ab43bc5648b
sandeep KOTHAPALLI <ksandeep291999@gmail.com>
2026-06-28 16:46:55 +0000

    new

---
+++ models/example/gold_layer/totalorders_by_each_month.sql
@@ -0,0 +1,4 @@
+{{ config
+(materialized = "table")}}
+select count(order_id) as total_orders , month(order_purchase_timestamp) as months 
+from {{ref('orders_raw_v')}} group by months order by total_orders desc
